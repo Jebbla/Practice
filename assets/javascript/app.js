2c959145b4d8eb26809b59fc9d756969bdab0754
Vincent Romine <vinjrom@live.com>
2019-06-01 13:34:47 -0500

    Added Google Maps display

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,5 @@
 $(document).ready(function() {
-
-    // Granim
+    var zipCode;
     var granimInstance = new Granim({
         element: '#canvas-image-blending',
         direction: 'diagonal',
@@ -21,151 +20,87 @@ $(document).ready(function() {
             }
         }
     });
-
-    //  Materials
+    $("#selectID").change(function() {
+        var selected = $(this).find('option:selected');
+    })
     var materialsArr = [
-        "Appliances & Equipment",
-        "Asphalt",
-        "Automobiles",
-        "Batteries",
-        "Bottles & Cans",
-        "Bricks, Masonry, & Stone",
-        "Cardboard & Paper",
-        "Carpet Padding/Foam",
-        "Ceiling Tiles",
-        "Concrete", "Construction & Demolition Debris",
-        "Deconstructed & Salvaged Items",
-        "Dirt- Clean Fill",
-        "Donations",
-        "Drums, Barrels, & Buckets",
-        "Drywall- Clean/Unpainted",
-        "Drywall- Demo & Painted",
-        "eWaste- Electronics/TVs/Computers",
-        "Glass- Plate or Tempered",
-        "Green Waste- Yard/Landscape",
-        "Hauling Services",
-        "Hazardous Waste & Materials",
-        "Mattress",
-        "Metals- Ferrous",
-        "Metals- Non Ferrous",
-        "Oil- Commercial Grade",
-        "Oil- Food Grade",
-        "Paint",
-        "Plastics- Consumer Grade",
-        "Plastics- Industrial Grade",
-        "Porcelain",
-        "Rock, Sand, & Gravel",
-        "Roofing Asphalt",
-        "Tires",
-        "Trash",
-        "Wood- Clean Lumber/Pallets",
-        "Wood- Treated/Painted"
+        { name: "Air-Conditioners", id: 591 },
+        { name: "Aluminum-Cans", id: 70 },
+        { name: "Asphalt", id: 212 },
+        { name: "Batteries", id: 104 },
+        { name: "Cardboard", id: 40 },
+        { name: "Construction-Debris", id: 385 },
+        { name: "Latex-Paint", id: 418 },
+        { name: "Adult-Toys", id: 353 },
+        { name: "Mattresses", id: 226 },
+        { name: "Paint-Thinners", id: 191 },
+        { name: "Plastic-Bottle", id: 60 },
+        { name: "Porcelain-Products", id: 214 },
+        { name: "Sand", id: 386 },
+        { name: "Small-Appliances", id: 362 },
+        { name: "Tires", id: 5 },
+        { name: "Truck-Tires", id: 633 },
+        { name: "Vehicles", id: 267 },
+        { name: "Washer/Dryers", id: 573 },
     ]
-
     materialsArr.forEach(function(element) {
         var optionTag = $("<option>")
-        optionTag.text(element)
+        optionTag.text(element.name)
+        optionTag.attr('id', element.name)
+        optionTag.attr("materialId", element.id);
         $("#materials").append(optionTag)
     });
-
     var radiusArr = [
         "5", "10", "15", "25", "50"
     ]
-
     radiusArr.forEach(function(element) {
         var optionTag = $("<option>")
         optionTag.text(element)
         $("#radius").append(optionTag)
     });
-
-    var zipCode = "55407";
-    var materialID = 1;
+    // var zipCode = "55407";
+    // var materialID ="104";
     var latitude, longitude;
-
-    var earthQuery = function() {
-        var apiKey = "3fb6e10a90808f0d";
-        var queryURL = "https://cors-anywhere.herokuapp.com/https://api.earth911.com/earth911.getPostalData?postal_code=" + zipCode + "&country=US&api_key=" +
-            apiKey
-
-        console.log(queryURL);
-
-        $.ajax({
-            method: "GET",
-            url: queryURL,
-        }).then(function(response) {
-            console.log(response);
-            var parsedResponse = JSON.parse(response);
-            latitude = parsedResponse.result.latitude;
-            longitude = parsedResponse.result.longitude;
-            console.log("https://cors-anywhere.herokuapp.com/https://api.earth911.com/earth911.searchLocations?api_key=3fb6e10a90808f0d" +
-                "&latitude=" + latitude +
-                "&longitude=" + longitude +
-                "&material_id=" + materialID);
+    var earthQuery = function(materialIdfromPage) {
+            var apiKey = "3fb6e10a90808f0d";
+            var queryURL = "https://cors-anywhere.herokuapp.com/https://api.earth911.com/earth911.getPostalData?postal_code=" + zipCode + "&country=US&api_key=" +
+                apiKey
+            console.log(queryURL);
             $.ajax({
                 method: "GET",
-                url: "https://cors-anywhere.herokuapp.com/https://api.earth911.com/earth911.searchLocations?api_key=3fb6e10a90808f0d" +
+                url: queryURL,
+            }).then(function(response) {
+                console.log(response);
+                var parsedResponse = JSON.parse(response);
+                latitude = parsedResponse.result.latitude;
+                longitude = parsedResponse.result.longitude;
+                console.log("https://cors-anywhere.herokuapp.com/https://api.earth911.com/earth911.searchLocations?api_key=3fb6e10a90808f0d" +
                     "&latitude=" + latitude +
                     "&longitude=" + longitude +
-                    "&material_id=" + materialID,
-
-            }).then(function(result) {
-                //logic for actual location data
-                console.log(result);
-            })
-
-        });
-    }
-    earthQuery();
-
-    // >>>> Earth911 - API
-
-    materialsArr.forEach(function(element) {
-        var optionTag = $("<option>")
-        optionTag.text(element)
-        $("#materials").append(optionTag)
-    });
-
-    var radiusArr = [
-        "5", "10", "15", "25", "50"
-    ]
-
-    radiusArr.forEach(function(element) {
-        var optionTag = $("<option>")
-        optionTag.text(element)
-        $("#radius").append(optionTag)
-    });
-
-
-    var zipCode = "";
-
-    $('#submit').on("click", function(event) {
+                    "&material_id=" + materialIdfromPage);
+                $.ajax({
+                    method: "GET",
+                    url: "https://cors-anywhere.herokuapp.com/https://api.earth911.com/earth911.searchLocations?api_key=3fb6e10a90808f0d" +
+                        "&latitude=" + latitude +
+                        "&longitude=" + longitude +
+                        "&material_id=" + materialIdfromPage,
+                }).then(function(result) {
+                    //logic for actual location data
+                    console.log(result);
+                })
+            });
+        }
+        // earthQuery();
+    $("#submit").click(function(event) {
         event.preventDefault();
-
-        zipCode = $('#zipCode').val().trim();
-
-
-        console.log(zipCode);
-
-        earthQuery();
+        var val = $('#materials').val();
+        var option = $('#' + val);
+        var materialID = option.attr('materialId');
+        console.log(materialID)
+        zipCode = $('#inlineFormInputName2').val()
+        console.log('zipCode====', zipCode)
+        earthQuery(materialID);
+        $('#inlineFormInputName2').val('')
     })
-
-
-    var earthQuery = function() {
-        var apiKey = "3fb6e10a90808f0d";
-        var queryURL = "https://cors-anywhere.herokuapp.com/https://api.earth911.com/earth911.getPostalData?postal_code=" + zipCode + "&country=US&api_key=" +
-            apiKey
-
-        console.log(queryURL);
-
-        $.ajax({
-            method: "GET",
-            url: queryURL,
-        }).then(function(result) {
-            console.log(result);
-
-        });
-    }
-
-
 
 });
